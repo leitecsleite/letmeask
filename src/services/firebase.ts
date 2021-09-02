e2337c781942e869f04d4ea0105435a28e23eb5a
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2021-09-02 19:07:49 -0300

    AULA - 02

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,10 +1,11 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from 'firebase/app'
+import  firebase from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import 'firebase/auth';
 import 'firebase/database';
 // Your web app's Firebase configuration
+/*
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain:process.env.REACT_APP_AUTH_DOMAIN ,
@@ -14,8 +15,23 @@ const firebaseConfig = {
     messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID ,
     appId: process.env.REACT_APP_APP_ID
   }; 
+*/
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBtqD6tKyilu6wDn9eeqmij1aQRU2XX8CI",
+  authDomain: "letmeask-a3098.firebaseapp.com",
+  databaseURL: "https://letmeask-a3098-default-rtdb.firebaseio.com",
+  projectId: "letmeask-a3098",
+  storageBucket: "letmeask-a3098.appspot.com",
+  messagingSenderId: "823397530775",
+  appId: "1:823397530775:web:0e74a01341f518ceff3dd2"
+};
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig); 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database }
 
 
